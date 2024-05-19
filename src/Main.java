94c05cff5a0d5d48af6a208158057e2836234c29
Admin <v.i.p@bk.ru>
2024-05-19 18:54:25 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,40 @@ public class Main {
         System.out.println();
         System.out.println("Задача-1");
         int clientOS = 0;
-        if (clientOS == 1) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOS == 0) {
+        } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
         System.out.println();
         System.out.println("Задача-2");
+        int Year = 2015;
+        int clientOS2 = 1;
         int clientDeviceYear = 2024;
-        if (clientDeviceYear > 2015 && clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (clientDeviceYear <= 2015 && clientOS == 1) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        if (clientDeviceYear > 2015 && clientOS == 0) {
+        if (clientOS2 == 1 && clientDeviceYear < Year) {
+            System.out.println("Установите облегченую версию приложения для IOS по ссылке");
+        } else if (clientOS2 == 1 && clientDeviceYear >= Year) {
+            System.out.println("Установите  версию приложения для IOS по ссылке");
+        } else if (clientOS2 == 0 && clientDeviceYear < Year) {
             System.out.println("Установите  версию приложения для iOS по ссылке");
         } else if (clientDeviceYear <= 2015 && clientOS == 0) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            System.out.println("Установите облегченную версию приложения для Android  по ссылке");
+        } else if (clientOS2 == 0 && clientDeviceYear >= Year) {
+            System.out.println("Установите  версию приложения для Android по ссылке");
         }
         System.out.println();
         System.out.println("Задача-3");
         int year = 2024;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println("Год " + year + " высокосный");
+            System.out.println("Год " + year + " високосный");
         } else {
-            System.out.println("Год " + year + " не является высокосным");
+            System.out.println("Год " + year + " не является високосным");
         }
         System.out.println();
         System.out.println("Задача-4");
         int distanceDelivery = 95;
         int day = 1;
-        if (distanceDelivery < 20) {
+        if (distanceDelivery <= 20) {
             System.out.println("Потребуется дня " + day);
         } else if (distanceDelivery >= 20 && distanceDelivery < 60) {
             System.out.println("Потребуется дней " + (day + 1));
@@ -54,22 +57,22 @@ public class Main {
                 System.out.println(" Зима ");
                 break;
             case 3:
-                System.out.println(" Весна ");
+                System.out.println(" Весна  ");
                 break;
             case 4:
-                System.out.println(" Весна ");
+                System.out.println(" Весна пришла");
                 break;
             case 5:
-                System.out.println(" Весна ");
+                System.out.println(" Весна зелена ");
                 break;
             case 6:
-                System.out.println(" Лето пришло ");
+                System.out.println(" Лето  ");
                 break;
             case 7:
-                System.out.println(" Лето ");
+                System.out.println(" Лето пришло ");
                 break;
             case 8:
-                System.out.println(" Лето ");
+                System.out.println(" Лето жаркое ");
                 break;
             case 9:
                 System.out.println(" Золотая осень ");
@@ -85,7 +88,7 @@ public class Main {
                 break;
             default:
                 System.out.println(" Такого месяца не существует ");
-                break;// надеюсь ошибок будет мало)))!!!
+                break;
         }
 
     }
